d79b5c4af9dc06a6a3f0e79259f738afc156b65f
Voary <voary030@gmail.com>
2026-07-01 23:30:20 +0300

    components Navbar reussie

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import Navbar from "./components/Navbar"
 function App() {
   return (
     <div>
+      <div className="border solid p-5 md:px-[15%]">
       <Navbar/>
+      </div>
     </div>
   )
 }
