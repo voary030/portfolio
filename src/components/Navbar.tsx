8cd4d2a12227e6e744520b06a32115c5d97ca3ac
Voary <voary030@gmail.com>
2026-07-01 23:06:36 +0300

    ok

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,16 @@
+import { Container } from 'lucide-react'
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <a href="#">
+            <Container />
+            VOARY 
+            <span className="text-accent">DEV</span>
+        </a>
+    </div>
+  )
+}
+
+export default Navbar
